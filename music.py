983eefca9700b937a53e8ca50663804d9de9f9f4
binz0209 <63950442+binz0209@users.noreply.github.com>
2020-09-20 21:41:38 +0700

    Update music.py

--- music.py
+++ music.py
@@ -480,6 +480,59 @@ bot.add_cog(Music(bot))
 async def on_member_join(member):
     channel = bot.get_channel(734727263916130385)
     await channel.send("<a:DCC_zloading:747301624615272519> Chào mừng  " + member.mention + " đến với <a:DCC_zloading:747301624615272519>\n<a:DCC_zchecked2:747301626691321926>**DESTRUCTIVE CREATIONS | Community**<a:DCC_zchecked2:747301626691321926>\n<a:DCC_zchamthan:749879179386028123><#734919993073991711> để lấy màu siêu sịn\n<a:DCC_zchamthan:749879179386028123><#736992698619265154> để được quyền lợi\n<a:DCC_zchamthan:749879179386028123><#736991958467215370> để tránh bay màu\n                            <a:DCC_zwlc1:749879098817380362><a:DCC_zwlc2:749879144468185159>")
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+
+    if 'ok' in message.content:
+        await message.add_reaction('<a:DCC_zchecked3:747301627014152233>')
+    if 'Ok' in message.content:
+        await message.add_reaction('<a:DCC_zchecked3:747301627014152233>')
+    if 'OK' in message.content:
+        await message.add_reaction('<a:DCC_zchecked3:747301627014152233>')
+
+
+    if 'yeu' in message.content:
+        await message.add_reaction('<:DCC_yeune:747298048417464330>')
+    if 'love' in message.content:
+        await message.add_reaction('<:DCC_yeune:747298048417464330>')
+    if 'yêu' in message.content:
+        await message.add_reaction('<:DCC_yeune:747298048417464330>')
+    if 'iu' in message.content:
+        await message.add_reaction('<:DCC_yeune:747298048417464330>')
+    if 'luv' in message.content:
+        await message.add_reaction('<:DCC_yeune:747298048417464330>')
+
+    if 'chém' in message.content:
+        await message.add_reaction('<:DCC_znhapnhay:747301623231152180>')
+    if 'chem' in message.content:
+        await message.add_reaction('<:DCC_znhapnhay:747301623231152180>')
+    if 'chếm' in message.content:
+        await message.add_reaction('<:DCC_znhapnhay:747301623231152180>')
+    if 'Chém' in message.content:
+        await message.add_reaction('<:DCC_znhapnhay:747301623231152180>')
+    if 'Chem' in message.content:
+        await message.add_reaction('<:DCC_znhapnhay:747301623231152180>')
+    if 'Chếm' in message.content:
+        await message.add_reaction('<:DCC_znhapnhay:747301623231152180>')
+
+    if 'binz' in message.content:
+        await message.add_reaction('<:DCC_sohai2:747298047632867438>')
+    if 'Binz' in message.content:
+        await message.add_reaction('<:DCC_sohai2:747298047632867438>')
+    if 'bé tư' in message.content:
+        await message.add_reaction('<:DCC_sohai2:747298047632867438>')
+    if 'Bé Tư' in message.content:
+        await message.add_reaction('<:DCC_sohai2:747298047632867438>')
+    if 'be tu' in message.content:
+        await message.add_reaction('<:DCC_sohai2:747298047632867438>')
+    if 'Be tu' in message.content:
+        await message.add_reaction('<:DCC_sohai2:747298047632867438>')
+    if 'Be Tu' in message.content:
+        await message.add_reaction('<:DCC_sohai2:747298047632867438>')
+    if 'Bé tư' in message.content:
+        await message.add_reaction('<:DCC_sohai2:747298047632867438>')
 
 @bot.event
 async def on_ready():
